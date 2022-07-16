8c2d8bb4ce8b237265e717bc2790a4f0e1345cad
Timhung803 <timhkt@gmail.com>
2022-07-17 03:22:47 +1200

    Added navigation links and applied global styles o buttons and links

--- src/pages/PhoneConfirmation.js
+++ src/pages/PhoneConfirmation.js
@@ -1,17 +1,35 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import style from "../style/phoneConfirm.module.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import PhoneInput from "react-phone-number-input";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 export default function PhoneConfirmation() {
-    const [value, setValue] = useState();
+  const [value, setValue] = useState();
   return (
     <div className={style.phoneConfirmContainer}>
-        <h1>Enter your phone</h1>
-        <p>By entering your number, you're agreeing to our{" "}
+      <Link exact to="/" className={style.backBtn}>
+        <IoIosArrowBack />
+      </Link>
+      <h1>Enter your phone</h1>
+      <PhoneInput
+        international
+        defaultCountry="NZ"
+        value={value}
+        onChange={setValue}
+      />
+      <p>
+        By entering your number, you're agreeing to our{" "}
         <span>Terms of Service and Privacy Policy. </span>
         Thanks!
-        </p>
+      </p>
+      <Link
+        exact
+        to="/code_confirm"
+        className="primaryBtn d-flex align-items-center"
+      >
+        Next <IoIosArrowForward className="ms-1" />
+      </Link>
     </div>
   );
 }
-
