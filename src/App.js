3f7a6f6c9a1839aef1813ddeb9b1d6de185c0e3b
Timhung803 <timhkt@gmail.com>
2022-07-16 22:34:59 +1200

    Added PlanLayout for more Clubhouse look and added Buttons using React Router, created & styled PhoneComfirmation Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import PlanLayout from './pages/layouts/PlanLayout';
 import Welcome from './pages/Welcome';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import PhoneConfirmation from './pages/PhoneConfirmation';
 
 
 // <Panlayout>
 function App() {
   return (
-    <PlanLayout>
-      <Welcome /> 
-    </PlanLayout>
+    <Router>
+      <PlanLayout>
+        <Route exact path='/' component={Welcome} />
+        <Route exact path='/invite' component={PhoneConfirmation} />
+      </PlanLayout>
+    </Router>
     
   );
 }
