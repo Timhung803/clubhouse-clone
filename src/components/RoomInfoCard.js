e5aa985220fa4eaa6613f6bf703e7d54da4c3496
Timhung803 <timhkt@gmail.com>
2022-07-17 22:17:59 +1200

    Added more detail and data to the Room component

--- src/components/RoomInfoCard.js
+++ src/components/RoomInfoCard.js
@@ -1,20 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import style from "../style/roomCard.module.css"
-import data from "../data/roonCard.json"
+import React, { useState, useEffect } from "react";
+import style from "../style/roomCard.module.css";
+import data from "../data/roonCard.json";
+import { BsChatDots, BsChatDotsFill, BsFillFilePersonFill, BsFillPersonFill,BsFilePerson } from "react-icons/bs";
 
 export default function RoomInfoCard() {
   return (
     <>
-    {data.map((item) => (
+      {data.map((item) => (
         <div>
-            <div>
-                <div className={style.roomCardContainer}>
-                    <h6>{item.title}</h6>
-                    <h2>{item.sub_titlle}</h2>
+          <div>
+            <div className={style.roomCardContainer}>
+              <h6>{item.title}</h6>
+              <h2>{item.sub_titlle}</h2>
+              <div className={style.roomMembers}>
+                <div>
+                  <BsFillFilePersonFill />
+                  <BsFilePerson />
                 </div>
+                <div>
+                  {item.members.map((item) => (
+                    <p>
+                      {item.first_name} {item.last_name} <BsChatDots />
+                    </p>
+                  ))}
+                  <p className='d-flex align-items-center'>
+                    <span className="me-2">1.8</span>
+                    <BsFillPersonFill />
+                    <span className="mx-2"></span>{" "}
+                    <span className="me-2">5</span><BsChatDotsFill />
+                  </p>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    ))}
+      ))}
     </>
-  )
+  );
 }
