d53637c6b773517b1026b386b7e3f7ef90f1c3cf
Timhung803 <timhkt@gmail.com>
2022-07-18 03:04:04 +1200

    Created and styled Bottom Buttoms and Sheets on the Home Screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import DailyInfoCard from "../components/DailyInfoCard";
 import Header from "../components/Header";
 import RoomInfoCard from "../components/RoomInfoCard";
 import style from "../style/home.module.css";
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsGrid3X3GapFill } from "react-icons/bs";
+import data from "../data/roonCard.json";
+import BottomSheet from "../components/BottomSheet";
 
 export default function Home() {
+  const [itemVisible, setItemsVisible] = useState(true);
+  const [sheetVisible, setSheetVisible] = useState(false);
+  const [sheetCreateRoom, setSheetCreateRoom] = useState(false);
+  const [loaderVisibility, setLoaderVisibility] = useState(false);
+  const [cardId, setCardId] = useState(1);
   return (
     <>
+      {loaderVisibility ? (
+        <div
+          style={{
+            position: "fixed",
+            top: "0",
+            right: "0",
+            bottom: "0",
+            left: "0",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <img src="icons8-dots-loading.gif" />
+        </div>
+      ) : (
+        ""
+      )}
       <Header />
       <div className={style.home_container}>
         <DailyInfoCard />
         <RoomInfoCard />
       </div>
+      <div className={style.action_btn}>
+        <button onClick={() => setSheetVisible(true)}>
+          <AiOutlinePlus className="me-2" />
+          Start a room
+        </button>
+        <button>
+          <BsGrid3X3GapFill />
+        </button>
+      </div>
+      <BottomSheet
+        sheetTittle="start room"
+        setSheetVisible={(item) => setSheetVisible(item)}
+        sheetVisible={sheetVisible}
+        cardDetail={data.find((item) => item.id == cardId)}
+        setItemsVisible={(item) => setItemsVisible(item)}
+        setSheetCreateRoom={(item) => {
+          setLoaderVisibility(true);
+          setTimeout(() => {
+            setSheetCreateRoom(item);
+            setLoaderVisibility(false);
+          }, 1000);
+        }}
+      />
     </>
   );
 }
