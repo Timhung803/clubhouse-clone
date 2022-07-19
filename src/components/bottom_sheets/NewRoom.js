e946be019e3d43fed157e139a4a0271815462fa1
Timhung803 <timhkt@gmail.com>
2022-07-20 04:26:09 +1200

    Added other style on the New Room Page

--- src/components/bottom_sheets/NewRoom.js
+++ src/components/bottom_sheets/NewRoom.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import style from "../../style/roomDetail.module.css";
 import { AiOutlineFile } from "react-icons/ai";
-import { BsMicMuteFill, BsFillFilePersonFill } from "react-icons/bs";
+import { BsMicMuteFill, BsMicFill, BsFillFilePersonFill, BsFillPersonCheckFill } from "react-icons/bs";
 import { IoIosArrowBack } from "react-icons/io";
+import { FaRegHandPeace, FaRegHandPaper } from "react-icons/fa"
+import { Card } from "react-bootstrap";
 
 export default function NewRoom(props) {
-  const [micMuteVisible, setMicMuteVisible] = useState(false);
+  const [micMuteVisible, setMicMuteVisible] = useState(true);
   const card = props.cardDetail;
   return (
     <>
@@ -24,7 +26,30 @@ export default function NewRoom(props) {
           </div>
           <div>
             <AiOutlineFile />
-            <BsFillFilePersonFill className={style.person_icon}/>
+            <BsFillFilePersonFill className={style.person_icon} />
+          </div>
+        </div>
+        <div className={style.roomDetailCard}>
+          <div
+            className="d-flex align-items-center justify-content-between flex-wrap"
+            style={{ padding: "0.5em 1em" }}
+          >
+            {card.members.map((item)=> (
+              <div className={style.memberContainer}>
+                {micMuteVisible ? (
+                  <div className={style.audio_icon}>
+                    <BsMicMuteFill />
+                  </div>
+                ) : (
+                  ""
+                )}
+                <BsFillPersonCheckFill className={style.personcheck_icon}/>
+                <p>
+                    <span>*</span>
+                    {item.first_name}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
